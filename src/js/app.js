ca50abdfac1d477979b871cf60988821d13bf36c
brendamesias <brendamesiasvillar@gmail.com>
2018-02-15 10:42:35 -0500

    subiendo cambios

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+let input = $('search');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  responseContainer.innerHTML = '';
+  searchForText = searchFiled.value;
+
+});
